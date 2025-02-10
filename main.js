7832b8f11444a998504b88921911236369a93bae
Oludare Suulola <osuulola@yahoo.com>
2025-02-10 14:18:07 +0000

    seccond

--- main.js
+++ main.js
@@ -4,37 +4,46 @@ const ten = document.getElementById('ten');
 const fifteen = document.getElementById('fifteen');
 const twofive = document.getElementById('twofive');
 const fifty = document.getElementById('fifty');
+const custom = document.getElementById('custom');
+
+const tip = document.getElementById('tip');
+const total = document.getElementById('total');
 
 const bill = document.getElementById('bill');
+const people = document.getElementById('people');
 
-let percent;
+let pen = Number(bill.value);
+let person = Number(people.value);
 
-bill.addEventListener('click', ()=>{
-    console.log(bill.value);
+ five.addEventListener('click', ()=>{
+    precent = Number(bill.value) * 0.05;
+    console.log(precent)
+    tip.innerText = precent/person;
 });
-
-const fiv = five.addEventListener('click', ()=>{
-    precent = 0.05*bill.value;
-    return percent;
-});
-ten.addEventListener('click', ()=>{
+pen = ten.addEventListener('click', ()=>{
     precent = 0.1 * bill.value;
     return precent;
 });
-fifteen.addEventListener('click', ()=>{
+pen = fifteen.addEventListener('click', ()=>{
     precent = 0.2 * bill.value;
     return precent;
 });
-twofive.addEventListener('click', ()=>{
+pen = twofive.addEventListener('click', ()=>{
     precent = 0.25 * bill.value;
     return precent;
 })
-fifty.addEventListener('click', ()=>{
+pen = fifty.addEventListener('click', ()=>{
     precent = 0.5 * bill.value;
     return precent;
 })
+custom.addEventListener('click', ()=>{
+    edit(custom.innerText);
+})
 
 function handleClick(){
+    tip.value = pen;
+}
 
-    return 
+function edit(text){
+    text = '';
 }
