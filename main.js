3198290177400ea9a3d67685e26465135c7fbcd5
Oludare Suulola <osuulola@yahoo.com>
2025-02-10 15:47:51 +0000

    second again

--- main.js
+++ main.js
@@ -12,49 +12,46 @@ const total = document.getElementById('total');
 const bill = document.getElementById('bill');
 const people = document.getElementById('people');
 
-const reset = document.getElementsByTagName('button');
+const reset = document.getElementById('reset');
 
-let person;
+reset.addEventListener('click', ()=>{
+    bill.value = '';
+    people.value = '';
+    tip.innerText = '$0.00', 
+    total.innerText = '$0.00';
+})
+
+let precent, person;
+ 
 
 
  five.addEventListener('click', ()=>{
     precent = Number(bill.value) * 0.05;
     person = Number(people.value);
-    tip.innerText = (precent/person).toFixed(2);
-    total.innerText += tip.innerText;
+    tip.innerText = '$'+(precent/person).toFixed(2);
+    // total.innerText = Number(total.innerText)+ Number(tip.innerText);
 });
 ten.addEventListener('click', ()=>{
     precent = Number(bill.value) * 0.1;
     person = Number(people.value);
-    tip.innerText = (precent/person).toFixed(2);
-    total.innerText += tip.innerText;
+    tip.innerText = '$'+(precent/person).toFixed(2);
+    // total.innerText += Number(tip.innerText).toFixed(2);
 });
 fifteen.addEventListener('click', ()=>{
     precent = Number(bill.value) * 0.15;
     person = Number(people.value);
-    tip.innerText = (precent/person).toFixed(2);
-    total.innerText += tip.innerText;
+    tip.innerText = '$'+(precent/person).toFixed(2);
 });
 twofive.addEventListener('click', ()=>{
     precent = Number(bill.value) * 0.25;
     person = Number(people.value);
-    tip.innerText = (precent/person).toFixed(2);
-    total.innerText += tip.innerText;
+    tip.innerText = '$'+(precent/person).toFixed(2);
 })
 fifty.addEventListener('click', ()=>{
     precent = Number(bill.value) * 0.5;
     person = Number(people.value);
-    tip.innerText = (precent/person).toFixed(2);
-    total.innerText += tip.innerText;
+    tip.innerText = '$'+(precent/person).toFixed(2);
 })
 custom.addEventListener('click', ()=>{
-    edit(custom.innerText);
-})
-
-function handleClick(){
-    
-}
-
-function edit(text){
-    text = '';
-}
+    edit(custom.innerText)
+});
